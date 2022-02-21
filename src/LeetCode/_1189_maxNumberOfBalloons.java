c177820df6e0566488b96e0fe7263f973d67a8a2
xiaoke <yjmqaz@126.com>
2022-02-21 08:51:31 +0800

    22/2/21

---
+++ src/LeetCode/_1189_maxNumberOfBalloons.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class _1189_maxNumberOfBalloons {
+    public static void main(String[] args) {
+        System.out.println(maxNumberOfBalloons("loonbalxballpoon"));
+    }
+
+    /**
+     * 执行用时：3 ms, 在所有 Java 提交中击败了72.84%的用户
+     * 内存消耗：39.7 MB, 在所有 Java 提交中击败了58.18%的用户
+     */
+    public static int maxNumberOfBalloons(String text) {
+        char[] ch = new char[26];
+        for (int i = 0; i < text.length(); i++) {
+            ch[text.charAt(i) - 'a']++;
+        }
+        int[] num = {ch[1], ch[0], ch['l' - 'a'] / 2, ch['o' - 'a'] / 2, ch['n' - 'a']};
+        return Arrays.stream(num).min().getAsInt();
+    }
+}
---
+++ src/LeetCode/_1984_minimumDifference.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+
+import java.util.Arrays;
+
+public class _1984_minimumDifference {
+    /**
+     * 执行用时：4 ms, 在所有 Java 提交中击败了99.98%的用户
+     * 内存消耗：41.4 MB, 在所有 Java 提交中击败了56.10%的用户
+     */
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int res = Integer.MAX_VALUE;
+        for (int l = 0, r = k - 1; r < nums.length; ) {
+            res = Math.min(res, nums[r++] - nums[l++]);
+        }
+        return res;
+    }
+}
