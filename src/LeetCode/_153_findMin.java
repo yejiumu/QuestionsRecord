79173d122d023523f8603fd6e23ecac4055fcb59
xiaoke <yjmqaz@126.com>
2022-04-23 10:12:58 +0800

    22/4/23
    Signed-off-by: xiaoke <yjmqaz@126.com>

--- src/LeetCode/_153_findMin.java
+++ src/LeetCode/_153_findMin.java
@@ -12,13 +12,30 @@ public class _153_findMin {
      * <p>
      * 题目要求应该是要用二分，可是数据量没必要😁
      */
+//    public int findMin(int[] nums) {
+//        int min = 5001;
+//        for (int num : nums) {
+//            if (num < min) {
+//                min = num;
+//            }
+//        }
+//        return min;
+//    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：41 MB, 在所有 Java 提交中击败了49.54%的用户
+     */
     public int findMin(int[] nums) {
-        int min = 5001;
-        for (int num : nums) {
-            if (num < min) {
-                min = num;
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return min;
+        return nums[left];
     }
 }
