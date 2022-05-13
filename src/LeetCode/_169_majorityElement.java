c9b08d36cfdaa7d5608a96311d751f32a8e8be3c
xiaoke <yjmqaz@126.com>
2022-05-13 14:43:10 +0800

    22/5/13
    Signed-off-by: xiaoke <yjmqaz@126.com>

---
+++ src/LeetCode/_169_majorityElement.java
@@ -0,0 +1,62 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class _169_majorityElement {
+    /**
+     * 执行用时：3 ms, 在所有 Java 提交中击败了33.61%的用户
+     * 内存消耗：44.5 MB, 在所有 Java 提交中击败了92.27%的用户
+     * <p>
+     * 想复杂了
+     */
+//    public int majorityElement(int[] nums) {
+//        int len = nums.length;
+//        if (len == 1) {
+//            return nums[0];
+//        }
+//        Arrays.sort(nums);
+//        int ans = 1;
+//        for (int i = 0; i < len; i++) {
+//            if (nums[i] == nums[i + 1]) {
+//                ans++;
+//            } else {
+//                ans = 1;
+//            }
+//            if (ans > len / 2) {
+//                return nums[i];
+//            }
+//        }
+//        return -1;
+//    }
+
+    /**
+     * 执行用时：2 ms, 在所有 Java 提交中击败了58.95%的用户
+     * 内存消耗：44.7 MB, 在所有 Java 提交中击败了71.96%的用户
+     * <p>
+     * 可是时间复杂度是NlogN，空间复杂度也要logN，因为自带排序就是这样
+     */
+//    public int majorityElement(int[] nums) {
+//        Arrays.sort(nums);
+//        return nums[nums.length / 2];
+//    }
+
+    /**
+     * 执行用时：2 ms, 在所有 Java 提交中击败了58.95%的用户
+     * 内存消耗：44.6 MB, 在所有 Java 提交中击败了86.83%的用户
+     *
+     * 利用过半的特点，两个两个不相同就相互抵消，剩下的一定是那个数
+     */
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
---
+++ src/LeetCode/_729_MyCalendar.java
@@ -0,0 +1,56 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+public class _729_MyCalendar {
+    /**
+     * 执行用时：78 ms, 在所有 Java 提交中击败了34.40%的用户
+     * 内存消耗：41.8 MB, 在所有 Java 提交中击败了91.66%的用户
+     * <p>
+     * 最普通的方法，直接遍历
+     */
+//    class MyCalendar {
+//        List<int[]> calendar;
+//
+//        public MyCalendar() {
+//            calendar = new ArrayList<>();
+//        }
+//
+//        public boolean book(int start, int end) {
+//            for (int[] tmp : calendar) {
+//                if (tmp[0] < end && tmp[1] > start) {
+//                    return false;
+//                }
+//            }
+//            calendar.add(new int[]{start, end});
+//            return true;
+//        }
+//    }
+
+    /**
+     * 执行用时：27 ms, 在所有 Java 提交中击败了45.70%的用户
+     * 内存消耗：41.9 MB, 在所有 Java 提交中击败了75.85%的用户
+     * <p>
+     * 没看懂
+     */
+    class MyCalendar {
+        TreeMap<Integer, Integer> calendar;
+
+        public MyCalendar() {
+            calendar = new TreeMap<>();
+        }
+
+        public boolean book(int start, int end) {
+            Integer prev = calendar.floorKey(start),
+                    next = calendar.ceilingKey(start);
+            if ((prev == null || calendar.get(prev) <= start) &&
+                    (next == null || end <= next)) {
+                calendar.put(start, end);
+                return true;
+            }
+            return false;
+        }
+    }
+}
