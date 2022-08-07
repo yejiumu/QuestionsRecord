8f370109a917d05c02f61b9b8c10ff7c5c5e923f
xiaoke <yjmqaz@126.com>
2022-08-07 10:32:03 +0800

    22/8/7
    Signed-off-by: xiaoke <yjmqaz@126.com>

--- src/LeetCode/_1688_numberOfMatches.java
+++ src/LeetCode/_1688_numberOfMatches.java
@@ -8,7 +8,7 @@ public class _1688_numberOfMatches {
      * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
      * 内存消耗：38.2 MB, 在所有 Java 提交中击败了5.01%的用户
      */
-    public static int numberOfMatches(int n) {
+    public static int numberOfMatches_(int n) {
         int result = 0;
         while (n != 1) {
             if (n % 2 == 0) {
@@ -21,5 +21,17 @@ public class _1688_numberOfMatches {
             }
         }
         return result;
+    }
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.4 MB, 在所有 Java 提交中击败了49.21%的用户
+     * <p>
+     * 这就是数学吗，太BT了
+     */
+    public static int numberOfMatches(int n) {
+        // 配对次数就是淘汰的队伍数
+        // 因为最后肯定是只剩下一支队伍，所以就直接n-1
+        return n - 1;
     }
 }
