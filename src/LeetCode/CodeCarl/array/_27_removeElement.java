2df91d1d25baf8c10f2c2300e605f92e6b348ee8
xiaoke <yjmqaz@126.com>
2022-10-18 23:50:26 +0800

    22/10/18
    Signed-off-by: xiaoke <yjmqaz@126.com>

---
+++ src/LeetCode/CodeCarl/array/_27_removeElement.java
@@ -0,0 +1,13 @@
+package LeetCode.CodeCarl.array;
+
+/**
+ * 移除元素
+ *
+ * @author xoke
+ * @date 2022/10/18
+ */
+public class _27_removeElement {
+    public int removeElement(int[] nums, int val) {
+
+    }
+}
