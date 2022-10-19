f29446c36a22d17339f2a8f1f22b44326671dc77
xiaoke <yjmqaz@126.com>
2022-10-19 23:38:15 +0800

    22/10/19
    Signed-off-by: xiaoke <yjmqaz@126.com>

--- src/LeetCode/CodeCarl/array/_977_sortedSquares.java
+++ src/LeetCode/CodeCarl/array/_977_sortedSquares.java
@@ -1,5 +1,7 @@
 package LeetCode.CodeCarl.array;
 
+import java.util.Arrays;
+
 /**
  * 有序数组的平方
  *
@@ -7,7 +9,17 @@ package LeetCode.CodeCarl.array;
  * @date 2022/10/19
  */
 public class _977_sortedSquares {
+    /**
+     * 执行用时：5 ms, 在所有 Java 提交中击败了24.45%的用户
+     * 内存消耗：43.2 MB, 在所有 Java 提交中击败了42.56%的用户
+     */
     public int[] sortedSquares(int[] nums) {
-
+        int len = nums.length;
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(arr);
+        return arr;
     }
 }
