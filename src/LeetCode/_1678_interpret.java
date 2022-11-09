35c2d4aa9969ba049ef7c4cb18321dd5dde004ca
xiaoke <yjmqaz@126.com>
2022-11-09 21:38:18 +0800

    22/11/9
    Signed-off-by: xiaoke <yjmqaz@126.com>

--- src/LeetCode/_1678_interpret.java
+++ src/LeetCode/_1678_interpret.java
@@ -3,6 +3,7 @@ package LeetCode;
 /**
  * 设计 Goal 解析器
  */
+@SuppressWarnings("all")
 public class _1678_interpret {
     /**
      * 执行用时：1 ms, 在所有 Java 提交中击败了55.24%的用户
@@ -16,33 +17,26 @@ public class _1678_interpret {
 //    }
     /**
      * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
-     * 内存消耗：39.1 MB, 在所有 Java 提交中击败了48.13%的用户
+     * 内存消耗：39.1 MB, 在所有 Java 提交中击败了85.29%的用户
      */
     public String interpret(String command) {
-        //空直接返回
-        if (command == null) {
-            return "";
-        }
-        int index = 0;
-        //String三兄弟最快的
-        StringBuilder sb = new StringBuilder();
-        //遍历command字符串
-        while (index < command.length()) {
-            //如果是"G"，向sb中添加"G"，并将index后移一位
-            if (command.charAt(index) == 'G') {
-                sb.append('G');
-                index++;
-                //如果后两位是"()"，则向sb中添加"o"，并将index后移两位
-            } else if (command.charAt(index) == '(' && command.charAt(index + 1) == ')') {
-                sb.append('o');
-                index = index + 2;
-                //如果第一位是"("，第二位不是")"，那肯定是"(al)"，则向sb中添加"al"，同时index后移四位
-            } else if (command.charAt(index) == '(' && command.charAt(index + 1) == 'a') {
-                sb.append("al");
-                index = index + 4;
+        int len = command.length();
+        StringBuilder res = new StringBuilder();
+        // 这里不用字符数组，直接charAt()也可以
+        char[] arr = command.toCharArray();
+        for (int i = 0; i < len; i++) {
+            if (arr[i] == 'G') {
+                res.append("G");
+            }else if (arr[i] == '(') {
+                if (arr[i + 1] == ')') {
+                    i++;
+                    res.append("o");
+                }else {
+                    i+=3;
+                    res.append("al");
+                }
             }
         }
-        //直接返回sb的字符串
-        return sb.toString();
+        return res.toString();
     }
 }
