9deadd170a60a755859168e4c063a86f4b9c3346
xiaoke <yjmqaz@126.com>
2022-11-20 15:37:38 +0800

    22/11/20
    Signed-off-by: xiaoke <yjmqaz@126.com>

--- src/LeetCode/WeekMatch/the_320/B.java
+++ src/LeetCode/WeekMatch/the_320/B.java
@@ -15,10 +15,23 @@ public class B {
 
     public List<List<Integer>> closestNodes(TreeNode root, List<Integer> queries) {
         travel(root);
+        int len = total.size();
         for (Integer query : queries) {
             List<Integer> temp = new ArrayList<>();
-            temp.add(uperK(query));
-            temp.add(downK(query));
+            // left为 <= query的最后一个，即 > query的第一个的左边一位
+            int left = search(len, query + 1) - 1;
+            if (left == len || left < 0) {
+                temp.add(-1);
+            } else {
+                temp.add(total.get(left));
+            }
+            // right为 >= query的第一个值
+            int right = search(len, query);
+            if (right == len) {
+                temp.add(-1);
+            } else {
+                temp.add(total.get(right));
+            }
             list.add(new ArrayList<>(temp));
         }
         return list;
@@ -33,44 +46,18 @@ public class B {
         travel(root.right);
     }
 
-    //获取值<=k的最大值
-    public int uperK(int k) {
-        int l = 0, r = total.size() - 1;
-        //标注1： 这里是l<r，
-        while (l < r) {
-            //标注2： 这样的操作是为了取高位
-            int mid = (l + r + 1) / 2;
-            if (total.get(mid) <= k) { //标注3：因为a[mid]<=k,所以a[mid]可能=k，所以mid坐标也满足条件，l = mid而不是mid+1;
-                l = mid;
+    // 寻找值为 >= target 的左边界
+    public int search(int len, int target) {
+        int left = 0, right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (total.get(mid) < target) {
+                left = mid + 1; // [mid + 1, right]
             } else {
-                r = mid - 1; //这是a[mid] > k的时候。
+                right = mid - 1;// [left, mid - 1]
             }
         }
-        //标注4： 因为此时求得到的是最接近于目标值k的数，
-        // 如果最小值都大于k的话，那么就没有办法得到了，所以就进行一个判断
-        if (total.get(l) > k) return -1;
-        //标注5： 其实这里无论返回 a[l] 还是a[r]都行，循环的退出时间是l == r 的时候
-        return total.get(l);
-    }
-
-
-    public int downK(int key) {
-        int low = 0, high = total.size() - 1;
-        while (low < high) {
-            //这里进行的是取低位， 也是为了使得循环可以正确退出，防止死循环
-            int mid = (low + high) / 2;
-            if (total.get(mid) < key) {
-                low = mid + 1;
-            } else { //a[mid] >= key
-                high = mid;   //因为mid也满足情况
-            }
-        }
-        //这里进行检查的原因参考上面的标注
-        if (total.get(high) >= key) {
-            return high;
-        } else {
-            return -1;
-        }
+        return left;
     }
 
 }
