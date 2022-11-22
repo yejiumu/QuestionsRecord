9ab2654e1fbf119fa9c94ea31e378c9e65fc2dc4
xiaoke <yjmqaz@126.com>
2022-11-22 10:51:11 +0800

    22/11/22
    Signed-off-by: xiaoke <yjmqaz@126.com>

--- src/LeetCode/CodeCarl/backtrack/_491_findSubsequences.java
+++ src/LeetCode/CodeCarl/backtrack/_491_findSubsequences.java
@@ -1,6 +1,8 @@
 package LeetCode.CodeCarl.backtrack;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -10,28 +12,39 @@ import java.util.List;
  * @date 2022/11/22
  */
 public class _491_findSubsequences {
-    List<List<Integer>> list = new ArrayList<>();
-    List<Integer> temp = new ArrayList<>();
+    /**
+     * 执行用时：5 ms, 在所有 Java 提交中击败了62.25%的用户
+     * 内存消耗：47.6 MB, 在所有 Java 提交中击败了34.14%的用户
+     */
+    //结果集合
+    List<List<Integer>> res = new ArrayList<>();
+    //路径集合
+    LinkedList<Integer> path = new LinkedList<>();
+
 
     public List<List<Integer>> findSubsequences(int[] nums) {
-        backing(nums, 0, 0);
-        return list;
+        getSubsequences(nums, 0);
+        return res;
     }
 
-    public void backing(int[] arr, int index, int count) {
-        if (count >= 2) {
-            list.add(new ArrayList<>(temp));
+    private void getSubsequences(int[] nums, int start) {
+        if (path.size() > 1) {
+            res.add(new ArrayList<>(path));
+            // 注意这里不要加return，要取树上的节点
         }
-        for (int i = index; i < arr.length; i++) {
-            if (i > index && arr[i] == arr[i - 1]) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = start; i < nums.length; i++) {
+            if (!path.isEmpty() && nums[i] < path.getLast()) {
                 continue;
             }
-            if (!temp.isEmpty() && arr[i] < temp.get(temp.size() - 1)) {
-                break;
+            // 当前层使用过了当前数字
+            if (map.getOrDefault(nums[i], 0) >= 1) {
+                continue;
             }
-            temp.add(arr[i]);
-            backing(arr, i + 1, count + 1);
-            temp.remove(temp.size() - 1);
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            path.add(nums[i]);
+            getSubsequences(nums, i + 1);
+            path.removeLast();
         }
     }
 }
